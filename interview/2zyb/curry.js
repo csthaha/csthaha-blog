70dd43d01191c302df341873f27c4826589f1b1c
chenst <chenst@yuanian.com>
2021-09-27 14:25:41 +0800

    yn

--- interview/2zyb/curry.js
+++ interview/2zyb/curry.js
@@ -11,6 +11,22 @@ function curry() {
     return addFun
 }
 
+// Function.prototype.toString()
+// 返回一个表示当前函数源代码的字符串
+```javasctipt
+function sum(a, b) {
+    return a + b;
+  }
+  
+  console.log(sum.toString());
+  // expected output: "function sum(a, b) {
+  //                     return a + b;
+  //                   }"
+  
+  console.log(Math.abs.toString());
+  // expected output: "function abs() { [native code] }"
+```
+
 console.log(curry(1,2))
 console.log(curry(1,2)(3,4))
 console.log(curry(1,2)(3)(4))
